451288dd382790e6fc3637ce32071ed7b021610b
harsayamani <harsayamani@gmail.com>
2020-07-20 12:38:46 +0700

    Revisi

--- app/Http/Controllers/DataProposalController.php
+++ app/Http/Controllers/DataProposalController.php
@@ -17,7 +17,7 @@ class DataProposalController extends Controller
         if(!Session::get('loginAdmin')){
             return redirect('/admin/login')->with('alert-danger', 'Anda harus login terlebih dahulu!');
         }else{
-            $proposal = DataProposal::orderBy('created_at', 'desc')->get();
+            $proposal = DataProposal::sortByAsc('status')->get();
             return view('admin/kelolaProposal', compact('proposal'));
         }
     }
